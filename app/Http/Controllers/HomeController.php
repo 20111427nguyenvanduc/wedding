0d2010c1277fd3cf7a2e2b212ec126198dfa49b8
20111427nguyenvanduc <20111427nguyenvanduc@gmail.com>
2023-12-27 15:21:45 +0700

    a

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -67,7 +67,7 @@ class HomeController extends Controller
 
             $bbox = imagettfbbox(45, 0, $font, $text);
 
-            $x = $bbox[0] + (imagesx($newImage) / 3) - ($bbox[4] / 2) - 200;
+            $x = $bbox[0] + (imagesx($newImage) / 3) - ($bbox[4] / 2) - 220;
 
 //            $y = $bbox[1] + (imagesy($newImage) / 2) - ($bbox[5] / 2) - 5;
 
