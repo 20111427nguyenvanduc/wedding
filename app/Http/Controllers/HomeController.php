6a316cfa8870a84668699f41a7f646bb1d0d0a78
20111427nguyenvanduc <20111427nguyenvanduc@gmail.com>
2024-04-23 21:55:31 +0700

    uodatee

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -96,7 +96,7 @@ class HomeController extends Controller
             $this->views['invitation'] = '/uploads/image/thiep-moi-cuoi-' . str_slug($text) . '.png';
             $this->views['link'] = '/' . $invitation->body;
         } else {
-            $this->views['invitation'] = '/duyetphuong/thiepcuoi1.png';
+            $this->views['invitation'] = '/duyetphuong/thiepcuoi1.jpg';
             $this->views['link'] = '/thiep-moi/';
         }
 
