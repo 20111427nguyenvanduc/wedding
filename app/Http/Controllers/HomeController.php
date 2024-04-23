3525c8bc7bdcd5456e72d949f195e3e069933e93
20111427nguyenvanduc <20111427nguyenvanduc@gmail.com>
2024-04-23 22:05:03 +0700

    uodatee

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -70,12 +70,12 @@ class HomeController extends Controller
 
 
             $x = 2000;
-            $y = 1070;
+            $y = 570;
             $font = public_path('assets/be-vietnam/BeVietnam-Medium.ttf');
 
             $bbox = imagettfbbox(20, 0, $font, $text);
 
-            $x = $bbox[0] + (imagesx($newImage) / 3) - ($bbox[4] / 2) - 530;
+            $x = $bbox[0] + (imagesx($newImage) / 3) - ($bbox[4] / 2) - 130;
 
 //            $y = $bbox[1] + (imagesy($newImage) / 2) - ($bbox[5] / 2) - 5;
 
